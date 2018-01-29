396639a245138e684f0a67c20f26b76684354f59
Deepak, Mk (623-Extern) <capgemini.deepak@daimler.com>
2018-01-29 19:11:44 +0530

    Initial commit

---
+++ GitlabCli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitlabCli
+//
+//  Created by Deepak, Mk (623-Extern) on 29/01/18.
+//  Copyright © 2018 Deepak, Mk (623-Extern). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
